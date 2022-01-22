5c5e5366fadd2b337dca11e484d625ed83d90141
Camila <42713856s@ies9023.net>
2022-01-22 16:17:03 -0300

    Agrego funcion para copiar texto y limpiar campos

---
+++ botonCopiar.js
@@ -0,0 +1,18 @@
+var botonLimpiar = document.querySelector(".btn-limpiar");
+var botonCopiar = document.querySelector(".btn-copiar");
+var resultado = document.querySelector("#resultado");
+var texto = document.querySelector(".texto");
+var seleccion = document.createRange();
+
+botonLimpiar.addEventListener("click",limpiar);
+botonCopiar.addEventListener("click",copiar);
+
+function limpiar(){
+    texto.value = "";
+    resultado.value = "";
+}
+function copiar (){
+    resultado.focus();
+    document.execCommand("selectAll");
+    document.execCommand("copy");
+};
